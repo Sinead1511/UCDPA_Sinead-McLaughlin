a906c216f5e8a84f4a382990f90d06e0a3740193
Sinead <sinead1511@hotmail.com>
2021-07-22 15:35:18 +0100

    Added datasets, sorted values, dropped rows, used loops to add column, cleansed data.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from typing import Any, Union
+from unittest import result
+
+from pandas import Series, DataFrame
+from pandas.core.generic import NDFrame
+from pandas.io.parsers import TextFileReader
+
+
 def my_function(stockp, stockp2):
     print("The Companies used for this anaysis of Stocks are " + stockp + " & "  + stockp2 +".")
 my_function(stockp="Twitter", stockp2="Facebook")
@@ -16,3 +24,33 @@ data = r.json()
 
 print(data)
 
+import pandas as pd
+
+Twit = pd.read_csv("facebook_data.csv")
+FB = pd.read_csv("twtr_data.csv")
+
+Merge = Twit.append(FB, sort=False)
+print(Merge)
+
+Merge.sort_values(by='adj_close', ascending=False, inplace=True)
+print(Merge)
+
+Merge.dropna(subset=['date'], inplace=True)
+print(Merge)
+
+result = []
+for value in Merge["adj_close"]:
+
+    if value >= 300:
+        result.append("$300+")
+    else:
+        result.append("<$300")
+
+Merge["Result"] = result
+print(Merge)
+
+Merge['adj_high'] = Merge['adj_high'].round(1)
+Merge['adj_low'] = Merge['adj_low'].round(1)
+Merge['adj_open'] = Merge['adj_open'].round(1)
+Merge['adj_close'] = Merge['adj_close'].round(1)
+print(Merge)
